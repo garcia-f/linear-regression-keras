d005d83029ed4e96885420ac08d4ebd498b2340f
Fabian <garciafabianemmanuel@gmail.com>
2024-09-02 21:58:41 -0300

    entrenando al modelo

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ from keras.api.optimizers import SGD
 # Cargar los datos desde el archivo CSV
 datos = pd.read_csv('altura_peso.csv', sep=',')
 
-
 # Extracción de las columnas 'Altura' y 'Peso'
 x = datos['Altura'].values
 y = datos['Peso'].values
 
-
 # Reemplazo de valores NaN, infinito positivo y negativo con la media
 x = np.nan_to_num(x, nan=np.nanmean(x), posinf=np.nanmean(x), neginf=np.nanmean(x))
 y = np.nan_to_num(y, nan=np.nanmean(y), posinf=np.nanmean(y), neginf=np.nanmean(y))
-
 
 # Normalización de los datos
 x_mean, x_std = np.mean(x), np.std(x)
@@ -28,17 +25,19 @@ y_mean, y_std = np.mean(y), np.std(y)
 x = (x - x_mean) / x_std
 y = (y - y_mean) / y_std
 
-
 # Configuración del modelo de regresión lineal
 np.random.seed(2)
 modelo = Sequential()
 modelo.add(Dense(1, input_dim=1, activation='linear')) 
 
-
 # Compilación del modelo con el optimizador SGD
 sgd = SGD(learning_rate=0.0004)
 modelo.compile(loss='mean_squared_error', optimizer=sgd)
 
-
 # Mostrar un resumen del modelo
 modelo.summary()
+
+# Entrenamiento del modelo
+num_epochs = 1000
+batch_size = len(x)  
+historial = modelo.fit(x, y, epochs=num_epochs, batch_size=batch_size, verbose=1)
